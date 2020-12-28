8a3976c1091b84b79e9b511eddb6ec7731f48612
Thomas William McClean <spam@tommcclean.me>
2020-12-28 23:32:01 +0000

    Block more tiles by default

--- src/pages/game.vue
+++ src/pages/game.vue
@@ -38,7 +38,7 @@ export default {
   mounted () {
     this.setupTiles()
 
-    this.setupBlockedTiles({ numberOfTiles: 5, excludedTiles: [34, 44, 54, 64, 37, 47, 57, 67] })
+    this.setupBlockedTiles({ numberOfTiles: 15, excludedTiles: [34, 44, 54, 64, 37, 47, 57, 67] })
 
     this.controlTiles({
       empire: 'Player 1',
