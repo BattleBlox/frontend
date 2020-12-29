711fe4010156adaf2b9de3ff05fec3174885dc5e
Thomas William McClean <spam@tommcclean.me>
2020-12-29 17:32:13 +0000

    Add Logo

---
+++ src/components/common/navigator.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="c-navigator">
+    <img src="/logo.png">
+  </div>
+</template>
+
+<style scoped>
+.c-navigator {
+  padding: 10px;
+}
+
+.c-navigator img {
+  width: 200px;
+}
+</style>
