85d1f71db512e88f95eaec7a5a079ccc5b9900a3
Thomas William McClean <spam@tommcclean.me>
2022-01-19 22:32:44 +0000

    Upgrade micro stat integration

--- plugins/statistics.client.js
+++ plugins/statistics.client.js
@@ -1,7 +1,7 @@
 import { Connection, Counter, HitCounter } from 'micro-stat'
 
 export default function (context, inject) {
-  Connection.connect('https://micro-stat-functions.azurewebsites.net/api/ingest?dsn=42250c66-6853-47df-85c3-b38e1af54bbb')
+  Connection.connect('42250c66-6853-47df-85c3-b38e1af54bbb')
 
   const statistics = {
     Counter,
